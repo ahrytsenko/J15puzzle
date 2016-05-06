831d4f33c3aa39e37d5591de816744f2608e3ba7
ahrytsenko <ahrytsenko@users.noreply.github.com>
2016-05-06 17:50:57 +0300

    Update J15puzzleGUI.java

--- src/J15puzzleGUI.java
+++ src/J15puzzleGUI.java
@@ -4,84 +4,115 @@
  *
  */
 
-package ua.kiev.agrit.java.j15puzzle;
+/*
+** Description: 15 Puzzle
+** Date: 10 MAY 2016
+** Author: Andrii Grytsenko
+** Programing Language: Java
+*/ 
 
-class J15puzzleGUI extends Canvas implements Runnable {
+package ua.kiev.agrit.games.j15puzzle;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/**
+ *
+ * @author Andrii Grytsenko (ahrytsenko@gmail.com)
+ */
+public class J15puzzleGUI extends JPanel {
     
-    private static final long serialVersionUID = 1L;
-    public static int WIDTH = 640; //ширина
-    public static int HEIGHT = 480; //высота 
-    public static String NAME = "15 PUZZLE"; //заголовок окна
-
-    public void run() {
-    	long lastTime = System.currentTimeMillis();
-    	long delta;
-    	
-    	init();
-    		
-    	while(running) {
-    		delta = System.currentTimeMillis() - lastTime;
-    		lastTime = System.currentTimeMillis();	
-    		update(delta);
-    		render();
-    	}
-    }
-    	
-    public void init() {
-    	//TODO
-    }
-    	
-    public void render() {
-    	BufferStrategy bs = getBufferStrategy(); 
-    	if (bs == null) {
-    		createBufferStrategy(2); //создаем BufferStrategy для нашего холста
-    		requestFocus();
-    		return;
-    	}
-    		
-    	Graphics g = bs.getDrawGraphics(); //получаем Graphics из созданной нами BufferStrategy
-    	g.setColor(Color.black); //выбрать цвет
-    	g.fillRect(0, 0, getWidth(), getHeight()); //заполнить прямоугольник 
-    	g.dispose();
-    	bs.show(); //показать
-    }
-
-    public void update(long delta) {
-    	//TODO
+    private final Dimension DRAUGHT_SIZE = new Dimension(40, 40);
+    private final Color DRAUGHT_BG_COLOR = new Color(0, 255, 0);
+    private final Color DRAUGHT_FG_COLOR = new Color(255, 255, 255);
+    private final int ROWS;
+    private final int COLS;
+    
+    public J15puzzleGUI(int rows, int cols, int space) {
+        super();
+        setLayout(new GridLayout(rows, cols, space, space));
+        ROWS = rows;
+        COLS = cols;
+        for (int i = 0; i < ROWS; i++)
+            for (int j = 0; j < COLS; j++) {
+                add(new Draught(DRAUGHT_SIZE, DRAUGHT_BG_COLOR, DRAUGHT_FG_COLOR, i*COLS+j+1));
+            }
     }
     
-    public void start() {
-	    running = true;
-	    new Thread(this).start();
+    public J15puzzleGUI() {
+        this(4, 4, 2);
     }
-
+    
     public static void main(String[] args) {
-    	J15puzzleGUI game = new J15puzzleGUI();
-    	game.setPreferredSize(new Dimension(WIDTH, HEIGHT));
-    
-    	JFrame frame = new JFrame(J15puzzleGUI.NAME);
-    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //выход из приложения по нажатию клавиши ESC
-    	frame.setLayout(new BorderLayout());
-    	frame.add(game, BorderLayout.CENTER); //добавляем холст на наш фрейм
-    	frame.pack();
-    	frame.setResizable(false);
-    	frame.setVisible(true);
-    
-    	game.start();
+        JFrame wnd = new JFrame("J15puzzle");
+        wnd.setLayout(new GridLayout(1, 1, 2, 2));
+        wnd.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        J15puzzleGUI content = new J15puzzleGUI();
+        wnd.setContentPane(content);
+        
+        wnd.pack();
+        wnd.setResizable(false);
+        wnd.setVisible(true);
     }
+    
+    private class Draught extends JPanel implements MouseListener, MouseMotionListener {
 
-    /*
-    public static void main(String[] args) {
+        private Dimension size;
+        private Color bgColor, fgColor;
+        private int number;
+        
+        public Draught(Dimension size, Color bgColor, Color fgColor, int number) {
+            super();
+            
+            this.size = size;
+            this.bgColor = bgColor;
+            this.fgColor = fgColor;
+            this.number = number;
+            
+            setBackground(bgColor);
+            setForeground(fgColor);
+            setSize(size);
+            setPreferredSize(size);
+            
+            addMouseListener(this);
+            addMouseMotionListener(this);
+        }
+        
+        @Override
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.drawString(Integer.toString(number), getWidth()/4, getHeight()/4*3);
+        }
+        
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            System.out.println("class Draught#" + number + ": mouseClicked");
+        }
 
-        J15puzzleGUI j15 = new J15puzzleGUI();
+        @Override
+        public void mousePressed(MouseEvent e) { }
 
-        JFrame f = new JFrame();
-        f.setSize(640,480);
-        f.add(j15);
-        f.setVisible(true);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        @Override
+        public void mouseReleased(MouseEvent e) { }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            System.out.println("class Draught#" + number + ": mouseEntered");
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+            System.out.println("class Draught#" + number + ": mouseExited");
+        }
+
+        @Override
+        public void mouseDragged(MouseEvent e) { }
+
+        @Override
+        public void mouseMoved(MouseEvent e) { }
         
     }
-    */
     
 }
